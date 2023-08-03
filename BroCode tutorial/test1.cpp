cba80560ab5377c6c3589f3f10de91138e3844bf
mzf11125 <mzidanfatonie@gmail.com>
2023-08-03 23:47:24 +0700

    Update test1.cpp

--- BroCode tutorial/test1.cpp
+++ BroCode tutorial/test1.cpp
@@ -2,7 +2,7 @@
 
 int main(){
 
-    printf("Hello programmers!\n");
-    printf("I like pizza too!");
+    printf("Hello programmers!\n");// This print 'hello programmers!'
+    printf("I like pizza too!");// This print 'i like pizza too'
     return 0;
 }
