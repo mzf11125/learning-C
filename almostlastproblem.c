8a882d1e30ce9b040eb1d14e4357c3d3ea24922d
mzf11125 <77999453+mzf11125@users.noreply.github.com>
2023-09-22 21:45:36 +0700

    Add files via upload

---
+++ almostlastproblem.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char id[11],name[100];
+    char class; 
+    int num;
+    scanf("%s", id);
+    scanf("%s", name);
+    scanf("%c", class);
+    scanf(" %[^\n]", name);
+    scanf(" %c %d", class, num);
+    printf("Id    : %s\n", id);
+    printf("Name  : %s\n", name);
+    printf("Class : %c\n", class);
+    printf("Num   : %d\n", num);
+
+    return 0;
+}
