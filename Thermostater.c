4271a31863c7aa6f33f46da6851ce233c96de68e
mzf11125 <mzidanfatonie@gmail.com>
2023-09-25 21:07:21 +0700

    add files

---
+++ Thermostater.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, step, M;
+    scanf("%d %d", &N, &M);
+    scanf("%d", &step); // Input the 'step' value
+
+        for (int i = N; i <= M + 1; i += step) { // Change the loop increment to 'step'
+            printf("%d\n", i);
+        }
+        
+    return 0;
+}
