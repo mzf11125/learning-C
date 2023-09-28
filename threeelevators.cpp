910fe6687759a3887f9da84bfe018ee1f75273ce
mzf11125 <mzidanfatonie@gmail.com>
2023-09-29 06:48:52 +0700

    add

--- threeelevators.cpp
+++ threeelevators.cpp
@@ -2,18 +2,16 @@
 
 int main() {
     long long x, y, k;
-    scanf("%d %d %d", &x, &y, &k);
+    scanf("%lld %lld %lld", &x, &y, &k);
 
-    
-    if (x < 0 || x > 100 || y < 0 || y > 100 || k < 0 || k > 100) {
+    if (x < 0LL || x > 100LL || y < 0LL || y > 100LL || k < 0LL || k > 100LL) {
         printf("Invalid input. Elevator floor numbers must be between 0 and 100.\n");
-        return 1; 
+        return 1;
     }
 
-    long long time = -1.00; 
+    long long time = -1LL;
 
     if ((x < k && y > k) || (x > k && y < k)) {
-        
         long long time_to_first, time_to_second;
 
         if (x < k) {
@@ -23,7 +21,7 @@ int main() {
         }
 
         if (y < k) {
-            time_to_second = k - y; 
+            time_to_second = k - y;
         } else {
             time_to_second = y - k;
         }
@@ -35,7 +33,7 @@ int main() {
         }
     }
 
-    printf("%.0.lld\n", time);
+    printf("%.0lld\n", time);
 
     return 0;
 }
---
+++ threeelevators3.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    long long X, Y, Z;
+    scanf("%lld %lld %lld", &X, &Y, &Z);
+
+    // Use while loops instead of for loops for your conditions.
+    while (X < 100) {
+       
+    }
+
+    while (Y > 0) {
+        
+    }
+
+    if (X == Z && Y == Z) {
+        printf("1\n");
+    } else {
+        printf("-1\n");
+    }
+
+    return 0;
+}
