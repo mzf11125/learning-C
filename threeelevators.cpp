dca80d6b71452f8ed70f1e2b1d57bb53971255fb
mzf11125 <77999453+mzf11125@users.noreply.github.com>
2023-09-29 07:34:56 +0700

    Update threeelevators.cpp

--- threeelevators.cpp
+++ threeelevators.cpp
@@ -1,39 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    long long x, y, k;
-    scanf("%lld %lld %lld", &x, &y, &k);
+    int x, y, k;
+    scanf("%d %d %d", &x, &y, &k);
 
-    if (x < 0LL || x > 100LL || y < 0LL || y > 100LL || k < 0LL || k > 100LL) {
-        printf("Invalid input. Elevator floor numbers must be between 0 and 100.\n");
-        return 1;
-    }
 
-    long long time = -1LL;
-
-    if ((x < k && y > k) || (x > k && y < k)) {
-        long long time_to_first, time_to_second;
+    if ((x <= k) && (y>=k)) {
+        int time_to_first;
 
         if (x < k) {
             time_to_first = k - x;
         } else {
             time_to_first = x - k;
         }
-
-        if (y < k) {
-            time_to_second = k - y;
-        } else {
-            time_to_second = y - k;
-        }
-
-        if (time_to_first < time_to_second) {
-            time = time_to_first;
-        } else {
-            time = time_to_second;
-        }
+        printf("%d\n",time_to_first);
+    }
+    else{
+        printf("-1\n");
     }
 
-    printf("%.0lld\n", time);
 
     return 0;
 }
