330c3fdc3ac96dafdf3e17b63a12381fbb5ddbec
mzf11125 <mzidanfatonie@gmail.com>
2023-10-17 21:44:11 +0700

    add

---
+++ testcassequiz1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+int main(){
+	int T, N;
+	
+	scanf("%d", &T);
+	scanf("%d", &N);
+
+	int result[T][N];	
+	for (int i = 0; i < T; i++){
+		for (int i = 0; i < N; i++){
+			scanf("%d ", &result[i]);
+			result[i] =+;
+		}	
+	}
+	printf("%d", result);
+}
