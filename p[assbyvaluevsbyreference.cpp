a902a1537374923e7cba5ecaf0f72c8d26e52f2a
mzf11125 <mzidanfatonie@gmail.com>
2023-10-24 14:13:33 +0700

    edit

--- p[assbyvaluevsbyreference.cpp
+++ p[assbyvaluevsbyreference.cpp
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+void swapByValue(int a, int b){
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("After swap A= %d\n", a);
+    printf("After swap b= %d\n", b);
+}
 
 int main(){
-    
-
-
+    // Two variable, a and b 
+    int a = 10;
+    int b = 5;
+    swapByValue(a,b);
+    printf("After A di main= %d\n", a);
+    printf("After b di main= %d\n", b);
     return 0;
 }
