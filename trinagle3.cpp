3e61c84a5eed77d6e367818297753c7b20c6784a
mzf11125 <mzidanfatonie@gmail.com>
2023-10-24 09:48:19 +0700

    yess ssiiiiiiiiiiiiiiiir   :)

--- trinagle3.cpp
+++ trinagle3.cpp
@@ -5,7 +5,7 @@ void drawRightTriangle(int N) {
 
     for (i = 0; i < N; i++) {
         for (j = 0; j < N - i - 1; j++) {
-            printf(" ");
+            printf("");
         }
         for (j = 0; j <= i; j++) {
             printf("*");
@@ -15,9 +15,11 @@ void drawRightTriangle(int N) {
 }
 
 int main() {
-    int M, N;
+    int M, N, T;
 
-    scanf("%d %d", &M, &N);
+    scanf("%d", &M, &N);
+    scanf("%d", &N);
+    scanf("%d", &T);
 
     for (int k = 0; k < M; k++) {
         drawRightTriangle(N);
