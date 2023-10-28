6f6cc3e4b62ba1ec8c2a24fe76d2a29a7522c683
mzf11125 <mzidanfatonie@gmail.com>
2023-10-28 14:57:51 +0700

    update

--- survivalbitwise.cpp
+++ survivalbitwise.cpp
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    int T;
+    int T, N, K;
 
     for(int i = 0; i < T; i++){
-        
+        scanf("%d", &N);
+        scanf("%d", &K);
+        for (int i = 0; i < N; i++) {
+            scanf("%d", &roomNumbers[i]);
+        }
     }
 
 
