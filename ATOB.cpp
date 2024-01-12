02463e93e7fa3bdb9d60023532f28095c3598afe
mzf11125 <mzidanfatonie@gmail.com>
2024-01-13 00:06:40 +0700

    update

---
+++ ATOB.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>|
+
+void calculate(int a, int b){
+	for(int )
+	if (a==1){
+		return a;
+	}
+	
+	
+}
+
+
+int main()
+{
+    int t, a, b, i;
+    scanf("%d", &t);
+    for (i = 1; i <= t; i++)
+    {
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: ", i);
+    }
+
+    return 0;
+}
--- getsomeresult12321321.cpp
+++ getsomeresult12321321.cpp
@@ -1,14 +1,30 @@
-//Get some results
-
-
 #include <stdio.h>
 
-
-int main(){
-	
-	
-	
-	
-	
-	return 0;
+int main() {
+    int t, a, b, i;
+    scanf("%d", &t);
+    for (i = 1; i <= t; i++) {
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: ", i);
+        if (a == b) {
+            printf("NO\n");
+            continue;
+        }
+        if (a == 1) {
+            a++;
+        }
+        while (a < b) {
+            if (b % 2 == 0 && b / 2 >= a) {
+                b /= 2;
+            } else {
+                b = b * 3 + 1;
+            }
+        }
+        if (a == b) {
+            printf("NO\n");
+        } else {
+            printf("YES\n");
+        }
+    }
+    return 0;
 }
