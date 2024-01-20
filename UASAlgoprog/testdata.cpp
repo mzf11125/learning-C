579e078b017770f0675d9271c4d1c03cae19be07
mzf11125 <mzidanfatonie@gmail.com>
2024-01-20 20:09:42 +0700

    UAS BINUS

---
+++ UASAlgoprog/testdata.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	FILE *fp = fopen("testdata.in", "r");
+    int negative_min = 0;
+    int positive_max = 0;
+    int number;
+
+    while (!feof(fp)) {
+    	fscanf(fp,"%d", &number);
+        if (number < 0) { // negative
+            if (negative_min > number) {
+                negative_min = number;
+            }
+        }
+        if (number > 0) { // positive
+            if (positive_max < number) {
+                positive_max = number;
+            }
+        }
+    }
+    printf("%d\n", positive_max - negative_min);
+    fclose(fp);
+    return 0;
+}
--- blajar UAS/test.cpp
+++ blajar UAS/test.cpp
@@ -1,1 +1,28 @@
-#include
+#include <stdio.h>
+
+int main()
+{
+    FILE *fp = fopen("testdata.in", "r");
+    int arr_neg[105], arr_pos[105];
+    int num;
+    int size;
+    while (!feof(fp))
+    {
+        fscanf(fp, "%d", "&num");
+        {
+            if (num < 0)
+            {
+                arr_neg[size] = num;
+                size++;
+            } // negative
+            else if (num > 0)
+            {
+                arr_pos[size] = num;
+                size++;
+            } // positive
+        }
+    }
+    for (int i = 0; i < size; i++)
+    {
+    }
+}
