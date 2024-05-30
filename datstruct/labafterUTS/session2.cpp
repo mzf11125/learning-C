2e7b426da9c453383b2342599641a8a9cb60cf72
mzf11125 <77999453+mzf11125@users.noreply.github.com>
2024-05-30 15:46:53 +0700

    add pseudocode

--- datstruct/labafterUTS/session2.cpp
+++ datstruct/labafterUTS/session2.cpp
@@ -1,10 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*pseudocode
+1. Enter from below
+2. If full, try to push
+3. If full or overflow, try to rotate
+4. If not full, return
+*/
 struct Node
 {
     int value;
     int height;
+    Node *left, *right;
 };
 
 int main()
